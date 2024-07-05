763f7b75040431f6b75492ddeea83a8ebee10d77
DT_Mayuraj <mayu.ranawat@dispatchtrack.com>
2024-07-05 12:43:25 +0530

    adding sorted hash key program

---
+++ script/sortedhash.rb
@@ -0,0 +1,9 @@
+players = {
+     "Rohit"= 35
+     "Hardik"=28
+     "Virat"=34
+     "Surya"=29
+    }   
+#sorting players name by sort_by method
+sorted_players = players.sort_by { |key, value| key.length }
+return sorted_players
