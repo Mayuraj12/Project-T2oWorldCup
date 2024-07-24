478be340f000f45d4a725abba92313b01fc37e26
DT_Mayuraj <mayu.ranawat@dispatchtrack.com>
2024-07-24 15:03:57 +0530

    adding match model

---
+++ app/models/match.rb
@@ -0,0 +1,17 @@
+require 'csv'
+class Match < ApplicationRecord
+  def self.import_from_csv(file_path)
+    CSV.foreach(file_path, headers: true) do |row|
+      Match.create!(
+        date: row['date'],
+        location: row['location'],
+        team1_id: row['team1_id'],
+        team2_id: row['team2_id'],
+        score_team1: row['score_team1'],
+        score_team2: row['score_team2']
+      )
+    end
+  rescue StandardError => e
+    Rails.logger.error "Failed to import CSV: #{e.message}"
+  end
+end
